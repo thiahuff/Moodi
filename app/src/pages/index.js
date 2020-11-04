52c5b4171d7b89f5504ee8fd694480f2966f9c01
Cynthia S <thiahuff@gmail.com>
2020-11-04 14:58:53 -0700

    Started on homepage register

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -5,18 +5,24 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const onSubmit = evt => {
+    evt.preventDefault()
+    console.log("Hello")
+  }
+  return (
+    <Layout>
+      <form onSubmit={onSubmit}>
+        <label>Email</label>
+        <input type="text" name="email" />
+
+        <label>password</label>
+        <input type="password" name="password" />
+
+        <button type="submit">Submit</button>
+      </form>
+    </Layout>
+  )
+}
 
 export default IndexPage
