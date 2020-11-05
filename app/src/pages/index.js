8a4a28ba9600b77143309dbfb91cd31014aa0057
Cynthia S <thiahuff@gmail.com>
2020-11-04 20:02:04 -0700

    set login event listener and tested

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,23 +1,45 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React, { useState } from "react"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => {
+  const [state, updateState] = useState({
+    email: "",
+    password: "",
+  })
+
+  const [isLoggedIn, setLoggedIn] = useState(false)
+
+  const onChange = evt => {
+    const { name, value } = evt.target
+    updateState({ ...state, [name]: value })
+  }
+
   const onSubmit = evt => {
     evt.preventDefault()
-    console.log("Hello")
+    console.log(state)
+
+    setLoggedIn(true)
   }
+
   return (
     <Layout>
+      {`Is logged in:${isLoggedIn}`}
       <form onSubmit={onSubmit}>
         <label>Email</label>
-        <input type="text" name="email" />
+        <input
+          type="text"
+          name="email"
+          value={state.email}
+          onChange={onChange}
+        />
 
         <label>password</label>
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          value={state.password}
+          onChange={onChange}
+        />
 
         <button type="submit">Submit</button>
       </form>
