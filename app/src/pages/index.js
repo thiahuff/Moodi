fbb09bff6151fe7222d43e29db380fc388189571
Cynthia S <thiahuff@gmail.com>
2020-11-05 11:55:24 -0700

    switched to react-hook-form to validate logins and registrations

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,22 +1,14 @@
 import React, { useState } from "react"
 import Layout from "../components/layout"
+import { useForm } from "react-hook-form"
 
 const IndexPage = () => {
-  const [state, updateState] = useState({
-    email: "",
-    password: "",
-  })
+  const { register, handleSubmit, errors } = useForm()
 
   const [isLoggedIn, setLoggedIn] = useState(false)
 
-  const onChange = evt => {
-    const { name, value } = evt.target
-    updateState({ ...state, [name]: value })
-  }
-
-  const onSubmit = evt => {
-    evt.preventDefault()
-    console.log(state)
+  const onSubmit = formValues => {
+    console.log(formValues)
 
     setLoggedIn(true)
   }
@@ -24,22 +16,12 @@ const IndexPage = () => {
   return (
     <Layout>
       {`Is logged in:${isLoggedIn}`}
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>Email</label>
-        <input
-          type="text"
-          name="email"
-          value={state.email}
-          onChange={onChange}
-        />
+        <input type="text" name="email" ref={register} />
 
         <label>password</label>
-        <input
-          type="password"
-          name="password"
-          value={state.password}
-          onChange={onChange}
-        />
+        <input type="password" name="password" ref={register} />
 
         <button type="submit">Submit</button>
       </form>
