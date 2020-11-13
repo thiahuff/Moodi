63fbcf539b99a0c778778267ff6e744fc4347cb2
Cynthia S <thiahuff@gmail.com>
2020-11-13 10:10:36 -0700

    Initial graph-view page setup

---
+++ app/src/pages/graph-view.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+{
+  /* <canvas id="myChart"></canvas>  
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script> */
+}
+
+const GraphView = () => (
+  <Layout>
+    <SEO title="Graph View" />
+    <h1>Graph View</h1>
+  </Layout>
+)
+
+export default GraphView
