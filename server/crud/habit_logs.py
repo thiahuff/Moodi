577dddf55ad8d0df9817dc638152358c65f4cd76
Cynthia S <thiahuff@gmail.com>
2020-11-13 08:48:12 -0700

    Added habit_logs endpoints and crud functions

--- server/crud/habit_logs.py
+++ server/crud/habit_logs.py
@@ -8,3 +8,33 @@ def create_habit_log(habit_log_instance):
     db.session.commit()
 
     return True
+
+
+def get_habit_logs_from_db():
+    return db.session.query(Habit_Log).all()
+
+
+def update_habit_log(habit_log):
+    db.session.query(Habit_Log).filter(Habit_Log.habit_log_id ==
+                                       habit_log['habit_log_id']).update(habit_log)
+    db.session.commit()
+
+
+def get_habit_logs_by_user(user_id):
+    """query for habit_logs by user_id."""
+    habit_logs = db.session.query(Habit_Log).filter(
+        Habit_Log.user_id == user_id).all()
+
+    return habit_logs
+
+
+def get_habit_log_by_id(habit_log_id):
+    habit_log = db.session.query(Habit_Log).filter(
+        Habit_Log.habit_log_id == habit_log_id).first()
+    return habit_log
+
+
+def delete_habit_log_by_id(habit_log_id):
+    db.session.query(Habit_Log).filter_by(habit_log_id=habit_log_id).delete()
+    db.session.commit()
+    return True
--- server/routes/habit_logs_routes.py
+++ server/routes/habit_logs_routes.py
@@ -1,8 +1,59 @@
 from app import app
+from crud.habit_logs import create_habit_log, get_habit_logs_from_db, update_habit_log, get_habit_logs_by_user, get_habit_log_by_id, delete_habit_log_by_id
 
 
-@app.route('/habit-logs', methods=['POST', 'GET'])
-def add_to_habit_log():
-    habit_log = HabitLog(**request.get_json())
-    result = create_habit_log(HabitLog)
-    return jsonify({"success": True})
+@app.route('/habit-logs', methods=['POST', 'GET', 'PUT'])
+def access_habit_log():
+    if request.method == 'POST':
+        return create_new_habit_log()
+    if request.method == 'GET':
+        return get_all_habit_logs()
+    if request.method == 'PUT':
+        return update_habit_logs()
+
+
+def create_new_habit_log():
+    request_body = request.get_json()
+    if isinstance(request_body, list):
+        for habit_log_json_obj in request_body:
+            habit_log = Habit_Log(**habit_log_json_obj)
+            create_habit_log(habit_log)
+    else:
+        habit_log = habit_Log(**request_body)
+        result = create_habit_log(habit_log)
+    return jsonify(request.get_json())
+
+
+def get_all_habit_logs():
+    habit_logs = get_habit_logs_from_db()
+    return jsonify([habit_log.serialize for habit_log in habit_logs])
+
+
+def update_habit_logs():
+    request_body = request.get_json()
+    if isinstance(request_body, list):
+        for habit_log in request_body:
+            if habit_log["habit_log_id"] is not None:
+                update_habit_log(habit_log)
+    else:
+        result = update_habit_log(request_body)
+
+    return jsonify(request.get_json())
+
+
+@app.route('/logs/user/<user_id>')
+def habit_logs_by_user_id(user_id):
+    habit_logs = get_habit_logs_by_user(user_id)
+    return jsonify([habit_log.serialize for habit_log in habit_logs])
+
+
+@app.route('/habit_logs/<habit_log_id>', methods=['GET', 'DELETE'])
+def habit_log_by_id(habit_log_id):
+    if request.method == 'GET':
+        habit_log = get_habit_log_by_id(habit_log_id)
+        if habit_log == None:
+            return jsonify(None)
+        return jsonify(habit_log.serialize)
+    else:
+        delete_habit_log_by_id(habit_log_id)
+        return jsonify({"success": True})
