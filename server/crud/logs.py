a88cc204847aac360c2c2759c36a7fcc131399c9
Cynthia S <thiahuff@gmail.com>
2020-11-13 08:24:10 -0700

    Added routes and crud functions for logs

--- server/crud/logs.py
+++ server/crud/logs.py
@@ -1,4 +1,4 @@
-from model import db, User, Habit, Habit_Log, Log, connect_to_db
+from model import db, Log
 
 
 def create_log(log_instance):
@@ -8,3 +8,31 @@ def create_log(log_instance):
     db.session.commit()
 
     return log_instance.log_id
+
+
+def get_logs_from_db():
+    return db.session.query(Log).all()
+
+
+def update_log(log):
+    db.session.query(Log).filter(Log.log_id ==
+                                 log['log_id']).update(log)
+    db.session.commit()
+
+
+def get_logs_by_user(user_id):
+    """query for logs by user_id."""
+    logs = db.session.query(Log).filter(Log.user_id == user_id).all()
+
+    return logs
+
+
+def get_log_by_id(log_id):
+    log = db.session.query(Log).filter(Log.log_id == log_id).first()
+    return log
+
+
+def delete_log_by_id(log_id):
+    db.session.query(Log).filter_by(log_id=log_id).delete()
+    db.session.commit()
+    return True
--- server/routes/logs_routes.py
+++ server/routes/logs_routes.py
@@ -1,8 +1,59 @@
 from app import app
+from crud.logs import create_log, get_logs_from_db, update_log, get_logs_by_user, get_log_by_id, delete_log_by_id
 
 
-@app.route('/logs', methods=['POST', 'GET'])
+@app.route('/logs', methods=['POST', 'GET', 'PUT'])
+def access_logs():
+    if request.method == 'POST':
+        return create_new_log()
+    if request.method == 'GET':
+        return get_all_logs()
+    if request.method == 'PUT':
+        return update_logs()
+
+
 def create_new_log():
-    log = Log(**request.get_json())
-    result = create_log(log)
-    return jsonify({"log_id": result})
+    request_body = request.get_json()
+    if isinstance(request_body, list):
+        for log_json_obj in request_body:
+            log = Log(**log_json_obj)
+            create_log(log)
+    else:
+        log = Log(**request_body)
+        result = create_log(log)
+    return jsonify(request.get_json())
+
+
+def get_all_logs():
+    logs = get_logs_from_db()
+    return jsonify([log.serialize for log in logs])
+
+
+def update_logs():
+    request_body = request.get_json()
+    if isinstance(request_body, list):
+        for log in request_body:
+            if log["log_id"] is not None:
+                update_log(log)
+    else:
+        result = update_log(request_body)
+
+    return jsonify(request.get_json())
+
+
+@app.route('/logs/user/<user_id>')
+def logs_by_user_id(user_id):
+    logs = get_logs_by_user(user_id)
+    return jsonify([log.serialize for log in logs])
+
+
+@app.route('/logs/<log_id>', methods=['GET', 'DELETE'])
+def log_by_id(log_id):
+    if request.method == 'GET':
+        log = get_log_by_id(log_id)
+        if log == None:
+            return jsonify(None)
+        return jsonify(log.serialize)
+    else:
+        delete_log_by_id(log_id)
+        return jsonify({"success": True})
