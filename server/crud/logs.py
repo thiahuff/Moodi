adf9b798a52c429feba8c2bf235359588ec66e53
Cynthia S <thiahuff@gmail.com>
2020-11-18 17:19:54 -0700

    Updated create_log function to only update mood_value rather than create new log if there is an existing one

--- server/crud/logs.py
+++ server/crud/logs.py
@@ -3,11 +3,20 @@ from model import db, Log
 
 def create_log(log_instance):
     """Create and return a new user."""
+    existing_log = db.session.query(Log).filter(
+        Log.user_id == log_instance.user_id, Log.date == log_instance.date).first()
+    print(existing_log)
+    if existing_log is not None:
+        print("log already exists!")
+        existing_log.mood_value = log_instance.mood_value
+        db.session.commit()
+        return existing_log
 
-    db.session.add(log_instance)
-    db.session.commit()
+    else:
+        db.session.add(log_instance)
+        db.session.commit()
 
-    return log_instance
+        return log_instance
 
 
 def get_logs_from_db():
@@ -36,3 +45,11 @@ def delete_log_by_id(log_id):
     db.session.query(Log).filter_by(log_id=log_id).delete()
     db.session.commit()
     return True
+
+
+def get_log_by_user_and_date(user_id, date):
+    """query for logs by user_id and date."""
+    log = db.session.query(Log).filter(
+        Log.user_id == user_id, Log.date == date).first()
+
+    return log
