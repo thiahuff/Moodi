bad41ab01938d6ff86bfc8141ddac5ea92d529d9
Cynthia S <thiahuff@gmail.com>
2020-11-22 14:19:15 -0700

    Started creating calendar component and basic styling

--- app/src/components/calendar.tsx
+++ app/src/components/calendar.tsx
@@ -1,20 +1,9 @@
 import dayjs from "dayjs"
 import React from "react"
 
-const getLeadingDays = (date: dayjs.Dayjs): number[] => {
-  const startDay = date.day() //0 -sunday => 6 saturday
-  const previousMonth = date.subtract(1, "month")
+import "./calendar.css"
 
-  return []
-}
-
-const getSucceedingDays = (date: dayjs.Dayjs): number[] => {
-  const nextMonth = date.add(1, "month")
-
-  return []
-}
-
-const Calendar = () => {
+const createCalendar = () => {
   const now = dayjs()
   const currentMonthDays: number = now.daysInMonth()
   const daysArray: number[] = []
@@ -23,9 +12,68 @@ const Calendar = () => {
     Array(currentMonthDays).keys()
   ).map((_, index) => index + 1)
 
-  return thisMonthsDatesArray.map((date, index) => {
-    return <div key={index}>{date}</div>
-  })
+  const leadingDays = getLeadingDays(now)
+  const succeedingDays = getSucceedingDays(now)
+
+  return createWeeks([
+    ...leadingDays,
+    ...thisMonthsDatesArray,
+    ...succeedingDays,
+  ])
+}
+
+const getLeadingDays = (date: dayjs.Dayjs): number[] => {
+  const leadingDays = date.startOf("month").day() // 0 -sunday => 6 saturday
+  const daysInPreviousMonth = dayjs(date).subtract(1, "month").daysInMonth()
+
+  let result: number[] = []
+
+  for (let index = 0; index < leadingDays; index++) {
+    result.push(daysInPreviousMonth - leadingDays + index)
+  }
+
+  return result
+}
+
+const getSucceedingDays = (date: dayjs.Dayjs): number[] => {
+  const endDate = date.endOf("month").day() // 0 -sunday => 6 saturday
+  const nextMonth = dayjs(date).add(1, "month").daysInMonth()
+
+  let result: number[] = []
+
+  for (let index = 0; index < 6 - endDate; index++) {
+    result.push(index + 1)
+  }
+
+  return result
+}
+
+const createWeeks = allDays => {
+  let result = []
+
+  for (let index = 0; index < allDays.length / 7; index++) {
+    result.push(allDays.slice(7 * index, 7 * index + 7))
+  }
+
+  return result
+}
+
+const Calendar = ({ logs }) => {
+  const weeks = createCalendar()
+
+  return (
+    <div className="calendar">
+      {weeks.map((week, index) => {
+        return (
+          <div key={index} className="calendar-week">
+            {week.map((day, index) => {
+              return <div className="calendar-day">{day}</div>
+            })}
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Calendar
--- app/src/pages/calendar.tsx
+++ app/src/pages/calendar.tsx
@@ -1,9 +1,30 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import Calendar from "../components/calendar"
+import Auth from "@aws-amplify/auth"
+import Axios from "axios"
 
 const CalendarView = () => {
-  return <Calendar />
+  // State to hold logs
+  const [logData, setLogData] = useState([])
+
+  // Logic to grab logs
+  const getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser()
+    const { data: logs } = await Axios.get(
+      `http://localhost:5000/logs/user/${user.username}`
+    )
+  }
+
+  useEffect(() => {
+    getUserData()
+  }, [])
+
+  return (
+    <Layout>
+      <Calendar logs={[]} />
+    </Layout>
+  )
 }
 
 export default CalendarView
